10db80ece57c14363e1c0f782efd3897f7304c9a
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2026-03-09 23:29:53 -0300

    our first laravel app

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+    <x-slot:title>
+        Welcome
+    </x-slot:title>
+    <div class="card bg-base-100 shadow max-w-xl mx-auto">
+        <div class="card-body">
+            <h1 class="card-title text-3xl mb-4">
+                Welcome to Chirper!
+            </h1>
+
+            <p>
+                This is your brand new Laravel app. Time to make it sing (or chirp!)
+            </p>
+        </div>
+    </div>
+</x-layout>
