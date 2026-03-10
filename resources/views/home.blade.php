7cc922cece7908799361a588d3a3eddd0a23851d
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2026-03-10 16:49:26 -0300

    our first cloud auto deployment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,17 +2,17 @@
     <x-slot:title>
         Welcome
     </x-slot:title>
-    <div class="card bg-base-100 shadow max-w-xl mx-auto">
-        <div class="card-body">
-            <h1 class="card-title text-3xl mb-4">
-                Welcome to Chirper!
-            </h1>
-
-            <p>
-                This is your brand new Laravel app. Time to make it sing (or chirp!)
-            </p>
-
-            <p class="mt-2 text-sm text-gray-600">Now live on the internet! 🎉</p>
-        </div>
+    <div class="max-w-2xl mx-auto">
+        @foreach($chirps as $chirp)
+            <div class="card bg-base-100 shadow mt-8">
+                <div class="card-body">
+                    <div>
+                        <div class="font-semibold">{{ $chirp['author'] }}</div>
+                        <div class="mt-1">{{ $chirp['message'] }}</div>
+                        <div class="text-sm text-gray-500 mt-2">{{ $chirp['time'] }}</div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
     </div>
 </x-layout>
