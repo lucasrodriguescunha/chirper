8827d441d12eb14bdc52267c2663c8c1db56e5f3
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2026-06-20 16:21:57 -0300

    feat: add search suggestion route to SearchController

--- routes/search.php
+++ routes/search.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\SearchController;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/search', [SearchController::class, 'index'])->name('search');
+    Route::get('/search/suggest', [SearchController::class, 'suggest'])->name('search.suggest');
 });
